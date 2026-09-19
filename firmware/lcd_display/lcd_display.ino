c54f4b62b382db09f71970398544b1cbe2d60eea
alicvo <avo0229@sdsu.edu>
2026-09-18 22:24:00 -0700

    refactored led toggle logic

--- firmware/lcd_display/lcd_display.ino
+++ firmware/lcd_display/lcd_display.ino
@@ -4,8 +4,7 @@
 // rs, rw, enable, D4, D5, D6, D7
 LiquidCrystal lcd(12, 11, 10, 5, 4, 3, 2);
 
-bool green_state = false;
-bool red_state = false;
+bool led_state = false;
 
 void print_msg(String msg1, String msg2) {
   // first row
@@ -21,27 +20,23 @@ void print_msg(String msg1, String msg2) {
   lcd.print(msg2.substring(0, 16));
 }
 
-void green_led(){
-  // pin D8
-  green_state = !green_state;
-  digitalWrite(8, green_state ? HIGH : LOW);
-}
+void led_toggle(){
+  led_state = !led_state;
 
-void red_led(){
-  // pin D7
-  red_state = !red_state;
-  digitalWrite(7, red_state ? HIGH : LOW);
+  // green led on first toggle call
+  digitalWrite(8, led_state ? HIGH : LOW);
+  // red led
+  digitalWrite(7, led_state ? LOW : HIGH);
 }
 
 void setup() {
   // initialize lcd (16 col, 2 rows)
   lcd.begin(16, 2);
-  Bridge.begin();
 
   // give python side the c++ functions
+  Bridge.begin();
   Bridge.provide("print_msg", print_msg);
-  Bridge.provide("green_led", green_led);
-  Bridge.provide("red_led", red_led);
+  Bridge.provide("led_toggle", led_toggle);
 }
 
 void loop() {
